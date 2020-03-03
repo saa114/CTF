3f2d754b043dd72b4f867c904ea2da3ff3888f68
saa114 <51106180+saa114@users.noreply.github.com>
2020-03-03 14:55:03 -0600

    Delete handler.py

--- handler.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-import signal, subprocess, os
-
-signal.alarm(1)
-tmpc = os.path.dirname(__file__)
-tmpc+='/questions.py'
-subprocess.Popen(['python',tmpc]).communicate()
